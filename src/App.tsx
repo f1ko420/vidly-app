43bbdbf4aa828b750a10c35460592ccd5b00c42f
FikoWeb <fiko.filiprac@gmail.com>
2022-07-19 13:14:56 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import MoviesTable from "./components/MoviesTable";
+
+function App() {
+  return (
+    <div className="App">
+      <MoviesTable />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MoviesTable.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MoviesTable = () => {
+  return (
+    <div>
+      <h1>Movies Table</h1>
+    </div>
+  );
+};
+
+export default MoviesTable;
